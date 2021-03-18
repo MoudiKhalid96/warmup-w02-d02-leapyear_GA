240e4e8c89da8fac5d738241a19138ca7c7df4d7
moudikhalid96 <moudi_khalid96@hotmail.com>
2021-03-18 03:55:21 +0300

    Warmup completed

--- solution.js
+++ solution.js
@@ -1,32 +1,26 @@
 /*
-Write a function that will take any given year and return whether it is a leap year or not. Remember that a leap year is:
-
-Every year that is evenly divisible by 4
-Except if it is evenly divisible by 100...
-Unless it is also divisible by 400
-Test Data... Make sure it is working!
+Write a function that will take any given year and return whether it is a leap year or not. Leap year is:
+- Every year that is evenly divisible by 4
+- Except if it is evenly divisible by 100
+- Unless it is also divisible by 400
+- Test Data... Make sure it is working!
 
 1997 is not a leap year, it should return false
 1996 is a leap year, it should return true
 1900 is not a leap year, it should return false
 2000 is a leap year, it should return true
-How to structure it...
-
-We want a custom function called isLeapYear
-Bonus!
-
-Ask the user what number they want to test
 */
 
-function isLeapYear(){
-    var year = prompt("Enter a year: ")
+function isLeapYear() {
+  var year = window.prompt("Enter a year: ")
 
-  if( ((year % 4 === 0) && (year % 100 !== 0))  || (year %  400 === 0)){
-    return true;   
-    }
-  else{
-     return false;
-    }
-}//end function
+  if (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(isLeapYear());
 
 
